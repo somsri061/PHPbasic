9bd354279d12335a3570dfd15f4a071727440bf5
somsri061 <6431503061@lamduan.mfu.ac.th>
2024-02-18 18:04:15 +0700

    ฟังก์ชั่นที่ทำงานเกี่ยวกับตัวแปร

---
+++ variablefunc.php
@@ -0,0 +1,23 @@
+<?php 
+$total1=null;
+$total2="";
+$total3=0;
+$total4="somsri";
+
+// unset($total4);
+/*
+echo "ค่าตัวแปล total1 =". is_null($total1)."<br>";
+echo "ค่าตัวแปล total2 =". is_null($total2)."<br>";
+echo "ค่าตัวแปล total3 =". is_null($total3)."<br>";
+echo "ค่าตัวแปล total4 =".is_null($total4)."<br>";
+*/
+echo var_dump($total1)."<br>";
+echo var_dump($total2)."<br>";
+echo var_dump($total3)."<br>";
+echo var_dump($total4)."<br>";
+
+
+// isset(true=1 false=0) unset empty is_null 
+// print_r()  แสดงค่าตัวแปร
+// var_dump() แสดงรายละเอรยดตัวแปร
+?>
